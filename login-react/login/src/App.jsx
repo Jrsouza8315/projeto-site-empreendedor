00e614cbcc92829527694642157bdc36457860aa
Jrsouza8315 <hbrcomercialssa@gmail.com>
2022-03-11 16:42:04 -0300

    api login react

---
+++ login-react/login/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+import LoginPage from './pages/LoginPages';
+import HomePage from './HomaPage';
+
+function App() {
+  return (
+    <div className="app">
+      <LoginPage/>
+      
+    </div>
+  );
+}
+
+export default App;
