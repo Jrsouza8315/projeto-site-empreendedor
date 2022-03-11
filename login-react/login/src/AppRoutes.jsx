925b4fc63c8a049e87f049a436139453aafa05c1
Jrsouza8315 <hbrcomercialssa@gmail.com>
2022-03-11 17:25:16 -0300

    routes

--- login-react/login/src/AppRoutes.jsx
+++ login-react/login/src/AppRoutes.jsx
@@ -1,1 +1,21 @@
-import {  }
+import {
+    BrowserRouter as Routers,
+    Route,
+    Routes,
+    Navigate
+} from "react-router-dom";
+
+import LoginPage from './pages/LoginPages';
+import HomePage from './HomaPage';
+
+const AppRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRoutes;
